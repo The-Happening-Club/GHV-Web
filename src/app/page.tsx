ac67b538954ef26413660b8543ca72da6f3184cb
Philipp Sanchez <philipp.sanchez@byteleaf.de>
2024-09-14 22:22:50 +0200

    Fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 import AboutUs from "@/components/lp-sections/AboutUs";
-import HeroSection from "@/components/lp-sections/HeroSection";
 import ServiceSection from "@/components/lp-sections/ServiceSection";
 
 
 export default function Home() {
   return (
     <div className="h-full">
-        <HeroSection />
         <ServiceSection />
         <AboutUs />
     </div>
