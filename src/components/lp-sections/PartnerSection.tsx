803f0cef14cce9526dc7273b879eeda769aa44ea
Philipp Sanchez <philipp.sanchez@byteleaf.de>
2024-09-17 15:20:37 +0200

    Landingpage Optimazation

--- src/components/lp-sections/PartnerSection.tsx
+++ src/components/lp-sections/PartnerSection.tsx
@@ -4,18 +4,42 @@ import React from "react";
 const PartnerSection = () => {
   return (
     //* hier sollen später die logos automatisch endlos durchlaufen */
-    <div className="flex w-full justify-center items-center gap-20 pt-4 pb-8 h-fit bg-slate-200">
-      <div className="flex justify-center items-center grayscale hover:grayscale-0">
-        <Image src="/logo/haufe_logo.svg" width={200} height={100} alt="" />
+    <div className="flex flex-col w-full min-h-48 justify-center items-center md:gap-20 pt-4 pb-8 h-fit bg-slate-200 overflow-x-scroll">
+      <div className="flex aspect-square justify-center items-center grayscale hover:grayscale-0">
+        <Image
+          src="/logo/haufe_logo.svg"
+          width={200}
+          height={100}
+          style={{ objectFit: "contain" }}
+          alt=""
+        />
       </div>
-      <div className="flex grayscale hover:grayscale-0">
-        <Image src="/logo/VDIV_Logo.png" width={200} height={100} alt="" />
+      <div className="flex grayscale aspect-square   hover:grayscale-0">
+        <Image
+          src="/logo/VDIV_Logo.png"
+          width={200}
+          height={100}
+          style={{ objectFit: "contain" }}
+          alt=""
+        />
       </div>
-      <div className="flex grayscale hover:grayscale-0">
-        <Image src="/logo/casavi_logo.png" width={200} height={100} alt="" />
+      <div className="flex grayscale aspect-square   hover:grayscale-0">
+        <Image
+          src="/logo/casavi_logo.png"
+          width={200}
+          height={100}
+          style={{ objectFit: "contain" }}
+          alt=""
+        />
       </div>
-      <div className="flex grayscale hover:grayscale-0">
-        <Image src="/logo/weinzierl_logo.png" width={200} height={100} alt="" />
+      <div className="flex grayscale aspect-square   hover:grayscale-0">
+        <Image
+          src="/logo/weinzierl_logo.png"
+          width={200}
+          height={100}
+          style={{ objectFit: "contain" }}
+          alt=""
+        />
       </div>
     </div>
   );
