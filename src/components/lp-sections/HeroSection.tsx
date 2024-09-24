f5eacf973bdefe0760fa87ff867e31b40394296c
Philipp Sanchez <philipp.sanchez@byteleaf.de>
2024-09-24 16:33:16 +0200

    fix main issues

--- src/components/lp-sections/HeroSection.tsx
+++ src/components/lp-sections/HeroSection.tsx
@@ -5,7 +5,7 @@ import Button from "../essentials/Button";
 const HeroSection = () => {
   return (
     <main className="flex md:min-h-[800px] w-full md:pb-0 justify-center bg-gradient-to-tl from-blue-dark to-blue-950 overflow-hidden px-6 xl:px-0">
-      <div className="md:relative md:max-w-[1366px] flex flex-col-reverse md:flex-row mt-14 pb-24 md:pb-0 md:pt-0 gap-8 md:gap-0">
+      <div className="lg:relative w-full lg:max-w-[1366px] flex flex-col-reverse xl:flex-row mt-14 pb-24 md:pb-0 md:pt-0 gap-8 md:gap-0">
         <div className="flex flex-col h-fit md:h-full md:w-7/12 md:z-10 md:flex-col-reverse">
           <div className="flex flex-col justify-center md:h-full gap-8 md:pl-10 lg:pl-0">
             <h1 className="text-slate-50 font-normalidad font-semibold text-4xl lg:text-6xl xl:text-8xl">
@@ -22,7 +22,7 @@ const HeroSection = () => {
             <Button prio={1} title="jetzt Anfragen!" href="/contact" />
           </div>
         </div>
-        <div className="md:absolute flex md:justify-end h-fit md:h-full md:w-full md:bottom-0 md:-right-20 xl:-right-48">
+        <div className="lg:absolute flex md:justify-end h-fit md:h-full md:w-full md:bottom-0 md:-right-20 xl:-right-48">
           <div className="md:flex md:items-end h-fit md:h-full lg:w-8/12">
             <Image
               src="/img/hero_sectio_img.png"
